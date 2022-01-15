c7ecc2d9f1fcbc50d390a7ccc0df1226a1f41a59
natali <natalinikulina1989@gmail.com>
2022-01-15 18:02:16 +0200

    change

---
+++ C_Sharp_HW19/PageObject/Body/BunnerStatic/BunnerStatic3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace C_Sharp_HW19.PageObject.Body
+{
+    public class BunnerStatic3:Body
+    {
+        public BunnerStatic3(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+    }
+}
---
+++ C_Sharp_HW19/PageObject/Body/BunnerStatic/BunnerStstic7.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+
+namespace C_Sharp_HW19.PageObject.Body
+{
+    public class BunnerStatic7:Body
+    {
+        public BunnerStatic7(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+    }
+}
