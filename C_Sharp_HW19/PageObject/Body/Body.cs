61b066f2ef470352caf6f7563615141e0389ac76
natali <natalinikulina1989@gmail.com>
2022-01-16 09:04:24 +0200

    changebody

--- C_Sharp_HW19/PageObject/Body/Body.cs
+++ C_Sharp_HW19/PageObject/Body/Body.cs
@@ -9,17 +9,22 @@ namespace C_Sharp_HW19.PageObjects
     {
         protected IWebDriver _driver;
 
-        private By _bunnerStatic1 = By.Id("");
-        private By _bunnerStatic2 = By.Id("");
-        private By _bunnerStatic3 = By.Id("");
-        private By _bunnerStatic4 = By.Id("");
-        private By _bunnerStatic5 = By.Id("");
-        private By _bunnerStatic6 = By.Id("");
-        private By _bunnerStatic7 = By.Id("");
+        private By _bunnerSale = By.XPath("//img[contains(@src,'http://automationpractice.com/modules/themeconfigurator/img/banner-img6.jpg')]");
+        private By _bunnerOnlyOnline = By.XPath("//img[contains(@src,'http://automationpractice.com/modules/themeconfigurator/img/banner-img7.jpg')]");
+        private By _bunnerTopTrends = By.XPath("//img[contains(@src,'http://automationpractice.com/modules/themeconfigurator/img/banner-img1.jpg')]");
+        private By _bunnerMens = By.XPath("//img[contains(@src,'http://automationpractice.com/modules/themeconfigurator/img/banner-img2.jpg')]");
+        private By _bunnerWomens = By.XPath("//img[contains(@src,'http://automationpractice.com/modules/themeconfigurator/img/banner-img3.jpg')]");
+        private By _bunnerSunGlasses = By.XPath("//img[contains(@src,'http://automationpractice.com/modules/themeconfigurator/img/banner-img4.jpg')]");
+        private By _bunnerSavvyTrends = By.XPath("//img[contains(@src,'http://automationpractice.com/modules/themeconfigurator/img/banner-img5.jpg')]");
 
-        private By _bunnerSlider1 = By.Id("");
-        private By _bunnerSlider2 = By.Id("");
-        private By _bunnerSlider3 = By.Id("");
+        private By _bunnerExcepteur1 = By.Id("");
+        private By _bunnerExcepteur2 = By.Id("");
+        private By _bunnerExcepteur3 = By.Id("");
+        private By _buttonShopNowExcepteur1 = By.XPath("");
+        private By _buttonShopNowExcepteur2 = By.XPath("");
+        private By _buttonShopNowExcepteur3 = By.XPath("");
+
+        private By _button = By.XPath("");
 
         private By _popular = By.Id("");
         private By _bestSellers = By.Id("");
