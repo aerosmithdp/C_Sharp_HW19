86edf2668ee8738f5a9dae79e8888843f7c69d8e
natali <natalinikulina1989@gmail.com>
2022-01-16 23:25:09 +0200

    changes

--- C_Sharp_HW19/PageObject/HeaderFolder/PersonalAccountInnerMenu/OrdersHistory.cs
+++ C_Sharp_HW19/PageObject/HeaderFolder/PersonalAccountInnerMenu/OrdersHistory.cs
@@ -1,31 +1,10 @@
-﻿using OpenQA.Selenium;
-using System;
-
-
-namespace C_Sharp_HW19.PageObjects
-
+﻿using System;
+namespace C_Sharp_HW19.PageObject.HeaderFolder.PersonalAccountInnerMenu
 {
-    public class OrderHistory : Header
+    public class OrdersHistory
     {
-        public OrderHistory(IWebDriver driver)
+        public OrdersHistory()
         {
-            _driver = driver;
-        }
-
-        private readonly By _goBackToMainMenu = By.XPath("//div[@id='center_column']/ul/li/a/span");
-        private readonly By _goBackToAccountMenu = By.XPath("//div[@id='center_column']/ul/li[2]/a/span");
-
-
-        public HomePage GoBackToMainMenuFromAccount()
-        {
-            _driver.FindElement(_goBackToMainMenu).Click();
-            return new HomePage(_driver);
-        }
-
-        public MyAccount GoBackToAccountMenuFromOrderHistoryPage()
-        {
-            _driver.FindElement(_goBackToAccountMenu).Click();
-            return new MyAccount(_driver);
         }
     }
 }
